93970e9adaa54d82678bb0f251b4901cdc71d351
Jan Korte <jan.korte@uni-oldenburg.de>
2016-06-02 10:17:52 +0200

    order works

--- order.py
+++ order.py
@@ -1,8 +1,18 @@
+import re
+import sys
+
 from antakya_connection import *
 
-with AntakyaConnection() as connection:
-    article_id = '00901'
-    connection.visit(BASE_URL + 'bestellen.jsp?artikel=' + article_id)
-    gebinde_count = '5'
-    connection.fill('gebinde', gebinde_count)
-    connection.find_by_name('UEBERNEHMEN').first.click()
+if __name__ == '__main__':
+    with open(sys.argv[1], 'r') as input_file:
+        # find first occurrence of 'Nummer    Menge   Name'
+        order = input_file.read().split('Nummer    Menge   Name', 1)[1]
+        with AntakyaConnection() as connection:
+            for line in order.split('\n'):
+                splitted_line = re.split(r' +', line)  # e.g. ['', '392459', '2', 'Bioland', 'Joghurt', 'Natur', '1L']
+                if len(splitted_line) > 3:
+                    article_id = splitted_line[1]
+                    gebinde_count = splitted_line[2]
+                    connection.visit(BASE_URL + 'bestellen.jsp?artikel=' + article_id)
+                    connection.fill('gebinde', gebinde_count)
+                    connection.find_by_name('UEBERNEHMEN').first.click()
