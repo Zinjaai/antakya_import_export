7fbbfe626795ebf7ace6c6850d478ed1b834277e
Jan Korte <jan.korte@uni-oldenburg.de>
2016-06-02 10:05:15 +0200

    crawler works

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# antakya_import_export
+
+### Install
+* Install Python 3: https://www.python.org/downloads/
+* Open up a command prompt:
+	* Windows: Search for 'cmd.exe'
+	* Linux, Mac: Search for 'Terminal'
+* Install Python modules by entering in the command prompt:
+	* ```pip3 install beautifulsoup4 splinter```
+
+### Run
+* Edit the login information inside the file 'antakya_connection.py'
+* Otherwise same use as FoodCoopOrderer 
--- crawl.py
+++ crawl.py
@@ -8,8 +8,8 @@ COLUMN_NAMES = ';{bestellnummer};{name};;;;{einheit};{preis};{mehrwertsteuer};{p
 mehrwertsteuer = '0'
 
 with AntakyaConnection() as connection:
-
     articles = ['empty row']
+    categories = set()
 
     connection.visit(BASE_URL + 'food.do?group=16')
     soup = BeautifulSoup(connection.html, 'html.parser')
@@ -22,22 +22,43 @@ with AntakyaConnection() as connection:
             link_elements = soup.find_all(href=re.compile('artikelinfo.jsp'))
             for link_element in link_elements:
                 bestellnummer = link_element.string
+                # item with same id exists
+                if any([bestellnummer in item for item in articles]):
+                    continue
 
                 name_element = link_element.parent.next_sibling.next_sibling
-                name = name_element.string.strip()[:59].replace('"', "'")  # cut too long names
 
                 gebinde_element = name_element.next_sibling.next_sibling
                 try:
-                    einheit, gebindegroesse = gebinde_element.string.split('x')
+                    gebindegroesse, einheit = gebinde_element.string.split('x')
                 except:
-                    einheit, gebindegroesse = 1, gebinde_element.string
+                    gebindegroesse, einheit = 1, gebinde_element.string
+
+                # compute bundles
+                if einheit == '5kg' or einheit == '25kg':
+                    gebindegroesse = einheit.replace('kg', '')
+                    einheit = '1kg'
+
+                name = name_element.string.strip().replace('"', "'")
+                append = ' ' + gebinde_element.string
+                # cut name if name is too long
+                if len(name + append) > 60:
+                    name = name[:(59 - len(append))]
+                name += append
+
+                # item with same name exists
+                if any([name in item for item in articles]):
+                    # todo:  get and append company
+                    # skipping is okay, because this case is rare (a tomato ketchup only)
+                    continue
 
                 preis_element = gebinde_element.next_sibling.next_sibling
                 preis = preis_element.string
 
                 pfand = '0'
 
-                kategorie = soup.find('td', class_='tab_th_norm', attrs={'colspan': 7}).contents[0].string
+                kategorie = soup.find('td', class_='tab_th_norm', attrs={'colspan': 7}).contents[0].string.strip()
+                categories.add(kategorie)
 
                 item = COLUMN_NAMES.format_map(vars())
                 articles.append(item)
@@ -45,3 +66,7 @@ with AntakyaConnection() as connection:
     with open('antakya_articles.csv', 'w') as file:
         for article in articles:
             file.write(article + '\n')
+
+    print('Create these categories in the foodsoft once:')
+    for c in categories:
+        print(c)
