6c65ef2d5e5f2eeb46b9da2271a1548fa620f7e8
Jan Korte <jan.korte@uni-oldenburg.de>
2016-07-21 08:24:53 +0200

    .

--- crawl.py
+++ crawl.py
@@ -1,4 +1,5 @@
 import re
+from decimal import Decimal
 
 from bs4 import BeautifulSoup
 
@@ -8,7 +9,7 @@ COLUMN_NAMES = ';{bestellnummer};{name};;;;{einheit};{preis};{mehrwertsteuer};{p
 mehrwertsteuer = '0'
 
 with AntakyaConnection() as connection:
-    articles = ['empty row']
+    articles = [COLUMN_NAMES]
     categories = set()
 
     connection.visit(BASE_URL + 'food.do?group=16')
@@ -27,19 +28,43 @@ with AntakyaConnection() as connection:
                     continue
 
                 name_element = link_element.parent.next_sibling.next_sibling
+                name = name_element.string.strip().replace('"', "'")
+
+                kategorie = soup.find('td', class_='tab_th_norm', attrs={'colspan': 7}).contents[0].string.strip()
+                categories.add(kategorie)
 
                 gebinde_element = name_element.next_sibling.next_sibling
-                try:
-                    gebindegroesse, einheit = gebinde_element.string.split('x')
-                except:
-                    gebindegroesse, einheit = 1, gebinde_element.string
+                einzel_preis_element = gebinde_element.next_sibling.next_sibling
+                einzel_preis = float(einzel_preis_element.string.replace(',', '.'))
+                gesamt_preis = float(einzel_preis_element.next_sibling.next_sibling.string.replace(',', '.'))
+                if re.match('\d*l', gebinde_element.string):
+                    gebindegroesse, _ = gebinde_element.string.split('l')
+                    einheit = '1l'
+                else:
+                    einheit = gebinde_element.string
+                    if einheit == '5Kg' or einheit == '25Kg' or einheit == '5kg' or einheit == '25kg' \
+                            or einheit == '5 kg' or einheit == '25 kg':
+                        gebindegroesse = einheit.replace('Kg', '').replace('kg', '')
+                        einheit = '1kg'
 
-                # compute bundles
-                if einheit == '5kg' or einheit == '25kg':
-                    gebindegroesse = einheit.replace('kg', '')
-                    einheit = '1kg'
+                    elif einheit == '1kg' and kategorie == 'Gewürze':
+                        gebindegroesse = 10
+                        einheit = '100g'
+                    else:
+                        gesamt_preis_mal10 = int('{:.2f}'.format(gesamt_preis).replace('.', ''))
+                        einzel_preis_mal10 = int('{:.2f}'.format(einzel_preis).replace('.', ''))
+                        if gesamt_preis_mal10 % einzel_preis_mal10 > 0:
+                            print('gesamt und einzelpreis passen nicht bei {} {} {}'.format(name, gesamt_preis,
+                                                                                            einzel_preis))
+                            raise Exception
+                        gebindegroesse = int(gesamt_preis_mal10 / einzel_preis_mal10)
+                        einheit = gebinde_element.string.replace('1x', '')
+                        if gebindegroesse != 1:
+                            if einheit.startswith('{}x'.format(gebindegroesse)):
+                                einheit = einheit.replace('{}x'.format(gebindegroesse), '')
+                            else:
+                                einheit = '{} / {}'.format(einheit, gebindegroesse)
 
-                name = name_element.string.strip().replace('"', "'")
                 append = ' ' + gebinde_element.string
                 # cut name if name is too long
                 if len(name + append) > 60:
@@ -52,13 +77,9 @@ with AntakyaConnection() as connection:
                     # skipping is okay, because this case is rare (a tomato ketchup only)
                     continue
 
-                preis_element = gebinde_element.next_sibling.next_sibling
-                preis = preis_element.string
+                preis = round(gesamt_preis / float(gebindegroesse), 2)
 
                 pfand = '0'
-
-                kategorie = soup.find('td', class_='tab_th_norm', attrs={'colspan': 7}).contents[0].string.strip()
-                categories.add(kategorie)
 
                 item = COLUMN_NAMES.format_map(vars())
                 articles.append(item)
