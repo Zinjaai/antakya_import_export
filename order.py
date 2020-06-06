fd6053b3193b67b38cc93a2a6c93e0c3e45d8729
knorx <daniel.reg@posteo.de>
2020-06-06 16:23:14 +0200

    antakya changed id format (more zero paddings required), use csv file

--- order.py
+++ order.py
@@ -1,18 +1,38 @@
-import re
+"""
+Import der zu bestellenden Waren aus der Foodsoft in die
+Antakya-Base Seite
+
+Dazu die Foodsoft Bestellübersicht (als csv) runterladen und
+die Datei hier als command-line-argument übergeben.
+"""
+
 import sys
 
 from antakya_connection import *
 
 if __name__ == '__main__':
-    with open(sys.argv[1], 'r') as input_file:
-        # find first occurrence of 'Nummer    Menge   Name'
-        order = input_file.read().split('Nummer    Menge   Name', 1)[1]
+    with open(sys.argv[1], 'r', encoding='latin1') as input_file:
+        order = input_file.read()
         with AntakyaConnection() as connection:
-            for line in order.split('\n'):
-                splitted_line = re.split(r' +', line)  # e.g. ['', '392459', '2', 'Bioland', 'Joghurt', 'Natur', '1L']
-                if len(splitted_line) > 3:
-                    article_id = splitted_line[1]
-                    gebinde_count = splitted_line[2]
-                    connection.visit(BASE_URL + 'bestellen.jsp?artikel=' + article_id)
-                    connection.fill('gebinde', gebinde_count)
-                    connection.find_by_name('UEBERNEHMEN').first.click()
+            for idx, line in enumerate(order.split('\n')):
+                if idx == 0:
+                    continue
+
+                splitted_line = line.split(';')
+                if len(splitted_line) <= 3:
+                    print('Uhm ignoring the row: ', line)
+                    continue
+
+                article_id = splitted_line[1]
+                gebinde_count = splitted_line[0]
+
+                url = BASE_URL + 'bestellen.jsp?artikel=' + article_id.zfill(5)
+                print('gebinde count', gebinde_count)
+                connection.visit(url)
+                connection.fill('gebinde', gebinde_count)
+                connection.find_by_name('UEBERNEHMEN').first.click()
+                print(f'Added {article_id}')
+
+#  http://217.86.233.218/FoodCoop/bestellen.jsp?artikel=905
+
+
