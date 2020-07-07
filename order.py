66b81defe74401ad8511dc6b2e33644745b7704c
knorx <daniel.reg@posteo.de>
2020-07-08 00:04:25 +0200

    decrease length of context manager, add variable name to filepath, improve docs

--- order.py
+++ order.py
@@ -4,6 +4,10 @@ Antakya-Base Seite
 
 Dazu die Foodsoft Bestellübersicht (als csv) runterladen und
 die Datei hier als command-line-argument übergeben.
+
+Am besten die csv datei noch einmal in einem texteditor öffnen und als latin-1
+kodierung abspeichern. (glaube es ging sonst schief)
+
 """
 
 import sys
@@ -11,27 +15,34 @@ import sys
 from antakya_connection import *
 
 if __name__ == '__main__':
-    with open(sys.argv[1], 'r', encoding='latin1') as input_file:
+
+    csv_path = sys.argv[1]
+
+    with open(csv_path, 'r', encoding='latin1') as input_file:
         order = input_file.read()
-        with AntakyaConnection() as connection:
-            for idx, line in enumerate(order.split('\n')):
-                if idx == 0:
-                    continue
 
-                splitted_line = line.split(';')
-                if len(splitted_line) <= 3:
-                    print('Uhm ignoring the row: ', line)
-                    continue
+    with AntakyaConnection() as connection:
+        for idx, line in enumerate(order.split('\n')):
+            if idx == 0:
+                continue
 
-                article_id = splitted_line[1]
-                gebinde_count = splitted_line[0]
+            print('\n\nLine', idx)
 
-                url = BASE_URL + 'bestellen.jsp?artikel=' + article_id.zfill(5)
-                print('gebinde count', gebinde_count)
-                connection.visit(url)
-                connection.fill('gebinde', gebinde_count)
-                connection.find_by_name('UEBERNEHMEN').first.click()
-                print(f'Added {article_id}')
+            splitted_line = line.split(';')
+            if len(splitted_line) <= 3:
+                print('Uhm ignoring the row: ', line)
+                continue
+
+            article_id = splitted_line[1]
+            gebinde_count = splitted_line[0]
+
+            url = BASE_URL + 'bestellen.jsp?artikel=' + article_id.zfill(5)
+            print('gebinde count', gebinde_count)
+            print(url)
+            connection.visit(url)
+            connection.fill('gebinde', gebinde_count)
+            connection.find_by_name('UEBERNEHMEN').first.click()
+            print(f'Added {article_id}')
 
 #  http://217.86.233.218/FoodCoop/bestellen.jsp?artikel=905
 
